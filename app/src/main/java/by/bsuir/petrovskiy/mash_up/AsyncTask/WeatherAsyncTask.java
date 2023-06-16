a06000265533a16683f85a81b63de55e7e88cfb1
Olegardor <olegardor@gmail.com>
2023-06-16 12:00:33 +0300

    1.75 Test fetchWeather()

---
+++ app/src/main/java/by/bsuir/petrovskiy/mash_up/AsyncTask/WeatherAsyncTask.java
@@ -0,0 +1,10 @@
+package by.bsuir.petrovskiy.mash_up.AsyncTask;
+
+import android.os.AsyncTask;
+import android.text.PrecomputedText;
+
+import androidx.loader.content.AsyncTaskLoader;
+
+public class WeatherAsyncTask extends AsyncTask<Void, Void, String> {
+
+}
